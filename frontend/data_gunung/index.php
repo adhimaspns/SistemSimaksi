b26cfd18715a016b39d5d4cb53e7c0a8603226b3
adhimaspns <adhimaspnsmjr1@gmail.com>
2021-03-23 05:18:14 +0700

    on progres crud data gunung (menampilkan data)

--- frontend/data_gunung/index.php
+++ frontend/data_gunung/index.php
@@ -8,6 +8,7 @@
 <body>
 
 <?php
+  include '../../database/koneksi.php';
   include '../layouts/sidebar.php';
 ?>
 
@@ -23,11 +24,34 @@
       <tr>
         <th>No</th>
         <th>Nama Gunung</th>
+        <th>Ketinggian</th>
         <th>Alamat Gunung</th>
-        <th>Satuan</th>
         <th>Harga HTM</th>
         <th>Aksi</th>
       </tr>
+      <?php
+        $no           = 1;
+        $selectData   = "SELECT * FROM data_gunung";
+        $queryData    = mysqli_query($host, $selectData);
+        while ($data  = mysqli_fetch_assoc($queryData) ) {
+
+      ?>
+      <tr>
+        <td><?= $no++ ?></td>
+        <td><?= $data['nama_gunung'] ?></td>
+        <td><?= $data['mdpl'] ?></td>
+        <td><?= $data['alamat_gunung'] ?></td>
+        <td><?= $data['unit_htm'] . " " . $data['satuan_htm'] . " " . "Rp. " . number_format($data['htm'],0,',','.')  ?></td>
+        <td>
+          <a href="edit.php?id=<?= $data['id_gunung']?>&page=datagunung" class="tmbl tmbl-kuning">
+            Edit
+          </a>
+          <a href="" class="tmbl tmbl-merah">
+            Hapus
+          </a>
+        </td>
+      </tr>
+      <?php } ?>
     </table>
   </div>
 </div>
