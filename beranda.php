aa5f3996a9edc0b276412cdb9d3a1bde5cc28736
adhimaspns <adhimaspnsmjr1@gmail.com>
2021-03-22 20:54:48 +0700

    update tampilan sidebar

--- beranda.php
+++ beranda.php
@@ -1,22 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
   <link rel="stylesheet" href="css/style.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
 </head>
 <body>
-  <?php
-    include 'frontend/layouts/sidebar.php';
-  ?>
-  <div class="s-layout">
-    <!-- Content -->
-    <main class="s-layout__content">
-      <h1>Full View, Please!</h1>
-    </main>
-  </div>
+
+<?php
+  include 'frontend/layouts/sidebar.php';
+?>
+
+<div class="content">
+  <h2>Responsive Sidebar Example</h2>
+
+</div>
+
 </body>
 </html>
--- frontend/layouts/sidebar.php
+++ frontend/layouts/sidebar.php
@@ -1,25 +1,6 @@
-<div class="s-layout__sidebar">
-  <a class="s-sidebar__trigger" href="#0">
-    <i class="fa fa-bars"></i>
-  </a>
-
-  <nav class="s-sidebar__nav">
-    <ul>
-      <li>
-        <a class="s-sidebar__nav-link" href="#0">
-          <i class="fa fa-home"></i><em>Beranda</em>
-        </a>
-      </li>
-      <li>
-        <a class="s-sidebar__nav-link" href="#0">
-          <i class="fa fa-user"></i><em>My Profile</em>
-        </a>
-      </li>
-      <li>
-        <a class="s-sidebar__nav-link" href="#0">
-          <i class="fa fa-camera"></i><em>Camera</em>
-        </a>
-      </li>
-    </ul>
-  </nav>
+<div class="sidebar">
+  <a class="active" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
 </div>
