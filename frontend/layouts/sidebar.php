27429f2b3c7959fd23c4f4f2ef3714529ba02221
adhimaspns <adhimaspnsmjr1@gmail.com>
2021-03-22 20:58:23 +0700

    update link sidebar

--- frontend/layouts/sidebar.php
+++ frontend/layouts/sidebar.php
@@ -1,6 +1,7 @@
 <div class="sidebar">
-  <a class="active" href="#home">Home</a>
-  <a href="#news">News</a>
-  <a href="#contact">Contact</a>
-  <a href="#about">About</a>
+  <a class="active" href="#home">Beranda</a>
+  <a href="#news">Data Gunung</a>
+  <a href="#contact">Simaksi</a>
+  <a href="#about">Laporan</a>
+  <a href="#about">Logout</a>
 </div>
