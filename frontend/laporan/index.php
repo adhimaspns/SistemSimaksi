ea12c60fd43ca66ad79367ee746fbccc6269b8e3
adhimaspns <adhimaspnsmjr1@gmail.com>
2021-03-27 18:03:28 +0700

    menambahkan laporan pendakian masuk & turun

---
+++ frontend/laporan/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../../css/style.css">
+  <title>Laporan</title>
+</head>
+<body>
+
+<?php
+  include '../../frontend/layouts/sidebar.php';
+?>
+
+<div class="content">
+  <h1>Pendataan Simaksi Gunung</h1>
+  <h2 class="teks-rata-kanan margin-top-100">Data Laporan</h2>
+
+  <center>
+    <a href="laporan_masuk.php?page=laporan" class="tmbl tmbl-biru">
+      laporan Pendaki Masuk
+    </a>
+    <a href="laporan_keluar.php?page=laporan" class="tmbl tmbl-hijau">
+      Laporan Pendaki Turun
+    </a>
+  </center>
+
+</div>
+
+</body>
+</html>
---
+++ frontend/laporan/laporan_keluar.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../../css/style.css">
+  <title>Laporan Pendaki Turun</title>
+</head>
+<body>
+
+<?php
+  include '../../frontend/layouts/sidebar.php';
+  include '../../database/koneksi.php';
+?>
+
+<div class="content">
+  <h1>Pendataan Simaksi Gunung</h1>
+  <h2 class="teks-rata-kanan margin-top-100">Data Laporan</h2>
+
+  <h3>Laporan Pendaki Turun</h3>
+  <div class="kotak-table">
+    <table class="table-responsive">
+      <tr>
+        <th>No</th>
+        <th>Nomor Simaksi</th>
+        <th>Nama Gunung</th>
+        <th>Tanggal Masuk</th>
+        <th>Tanggal Keluar</th>
+        <th>Jumlah Hari</th>
+        <th>Jumlah Pendaki</th>
+        <th>Tanggal Turun</th>
+      </tr>
+      <?php
+        $status         = "Masih Mendaki";
+        $no             = 1;
+        $selectLaporan  = "SELECT * FROM laporan INNER JOIN data_gunung ON laporan.gunung_id = data_gunung.id_gunung WHERE lprn_status_pendakian = '$status' ";
+        $queryLaporan   = mysqli_query($host, $selectLaporan);
+        while ($data  = mysqli_fetch_assoc($queryLaporan) ) {
+
+      ?>
+      <tr>
+        <td><?= $no++ ?></td>
+        <td><?= $data['nomor_simaksi'] ?></td>
+        <td><?= $data['nama_gunung'] ?></td>
+        <td><?= date('d M Y', strtotime($data['laporan_tgl_masuk'])) ?></td>
+        <td><?= date('d M Y', strtotime($data['laporan_tgl_keluar'])) ?></td>
+        <td><?= $data['laporan_jml_hari'] ?></td>
+        <td><?= $data['jumlah_pendaki'] . " " . "Orang" ?></td>
+        <td><?= date('d M Y -- H:i:s', strtotime($data['lprn_tgl_keluar'])) ?></td>
+      </tr>
+      <?php } ?>
+    </table>
+  </div>
+
+  <a href="index.php?page=laporan" class="tmbl tmbl-abu-abu margin-20-0">
+    Kembali
+  </a>
+
+</div>
+
+</body>
+</html>
