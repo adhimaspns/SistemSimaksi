2b0879107119c9f4ca33a9914f8ce35f22c48094
adhimaspns <adhimaspnsmjr1@gmail.com>
2021-03-27 17:39:54 +0700

    menambahkan link laporan

--- frontend/layouts/sidebar.php
+++ frontend/layouts/sidebar.php
@@ -2,6 +2,6 @@
   <a class="<?php if($_GET['page'] == 'beranda'){  ?>active <?php }?>" href="/PendataanSimaksiGunung/beranda.php?page=beranda">Beranda</a>
   <a class="<?php if($_GET['page'] == 'datagunung'){  ?>active <?php }?>" href="/PendataanSimaksiGunung/frontend/data_gunung/index.php?page=datagunung">Data Gunung</a>
   <a class="<?php if($_GET['page'] == 'simaksi'){  ?>active <?php }?>" href="/PendataanSimaksiGunung/frontend/simaksi/index.php?page=simaksi">Simaksi</a>
-  <a class="<?php if($_GET['page'] == 'laporan'){  ?>active <?php }?>" href="#about">Laporan</a>
+  <a class="<?php if($_GET['page'] == 'laporan'){  ?>active <?php }?>" href="/PendataanSimaksiGunung/frontend/laporan/index.php?page=laporan">Laporan</a>
   <a class="<?php if($_GET['page'] == 'logout'){  ?>active <?php }?>" href="#about">Logout</a>
 </div>
