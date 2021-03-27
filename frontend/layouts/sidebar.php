a201e6da1cd64adeb6ce6d3ef7cbd448cdd72e73
adhimaspns <adhimaspnsmjr1@gmail.com>
2021-03-27 19:10:50 +0700

    menambahkan link logout

--- frontend/layouts/sidebar.php
+++ frontend/layouts/sidebar.php
@@ -3,5 +3,5 @@
   <a class="<?php if($_GET['page'] == 'datagunung'){  ?>active <?php }?>" href="/PendataanSimaksiGunung/frontend/data_gunung/index.php?page=datagunung">Data Gunung</a>
   <a class="<?php if($_GET['page'] == 'simaksi'){  ?>active <?php }?>" href="/PendataanSimaksiGunung/frontend/simaksi/index.php?page=simaksi">Simaksi</a>
   <a class="<?php if($_GET['page'] == 'laporan'){  ?>active <?php }?>" href="/PendataanSimaksiGunung/frontend/laporan/index.php?page=laporan">Laporan</a>
-  <a class="<?php if($_GET['page'] == 'logout'){  ?>active <?php }?>" href="#about">Logout</a>
+  <a href="/PendataanSimaksiGunung/backend/login/logout.php">Logout</a>
 </div>
